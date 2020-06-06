b44693048b440517f19f01ffe9fb7df453e5cfce
Adam Przywuski <adamprzywuski98@gmail.com>
2020-06-06 19:45:32 +0200

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,34 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        try {
+            DataSource dataSource = new DataSource("credit.arff");
+        }
+        catch (Exception ex)
+        {
+            System.out.println("The dataset is incorrect or it can't be founded"+ex);
+        }
+
+
+    }
+}
