2dc64bb4934a4e576f397624fe252743cef23e50
Adam Przywuski <adamprzywuski98@gmail.com>
2020-07-01 17:38:24 +0200

    Cleaning code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import weka.core.converters.ConverterUtils.DataSource;
 
 
 public class Main extends Application {
 
-    private static DataSource dataSource;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
