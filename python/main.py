4c61a01617ee8a1f6b903f1fbdf966b96f777b51
matostr98 <matostr98@gmail.com>
2020-07-02 20:43:15 +0200

    added multilayer perceptron and classifiers hotfix

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.neural_network import MLPClassifier
 
 from models.classifier import classifier
 from models.data_preprocessing.german_credit import preprocess_german_credit
@@ -17,19 +18,24 @@ features_covid, labels_covid = preprocess_covid()
 # classifiers
 rf_classifier = RandomForestClassifier(n_estimators=100)
 ab_classifier = AdaBoostClassifier(n_estimators=100)
+mlp_classifier = MLPClassifier(random_state=1, max_iter=300)
 
 # ## random forest
 test_labels_gcd_rf, y_pred_gcd_rf, delta_gcd_rf = classifier(features_gcd, labels_gcd, rf_classifier)
-test_labels_glass_rf, y_pred_glass_rf, delta_glass_rf = classifier(features_glass, labels_glass, ab_classifier)
-
-## adaboost
-test_labels_gcd_ab, y_pred_gcd_ab, delta_gcd_ab = classifier(features_gcd, labels_gcd, rf_classifier)
-test_labels_glass_ab, y_pred_glass_ab, delta_glass_ab = classifier(features_glass, labels_glass, ab_classifier)
-
+test_labels_glass_rf, y_pred_glass_rf, delta_glass_rf = classifier(features_glass, labels_glass, rf_classifier)
 # test_labels_covid_rf, y_pred_covid_rf, delta_covid_rf = classifier(features_covid, labels_covid, rf_classifier)
 
+## adaboost
+test_labels_gcd_ab, y_pred_gcd_ab, delta_gcd_ab = classifier(features_gcd, labels_gcd, ab_classifier)
+test_labels_glass_ab, y_pred_glass_ab, delta_glass_ab = classifier(features_glass, labels_glass, ab_classifier)
+
+## multilayer perceptron
+test_labels_gcd_mlp, y_pred_gcd_mlp, delta_gcd_mlp = classifier(features_gcd, labels_gcd, mlp_classifier)
+test_labels_glass_mlp, y_pred_glass_mlp, delta_glass_mlp = classifier(features_glass, labels_glass, mlp_classifier)
+
+
 # results
-print("Random forest results:\n")
+print("\nRandom forest results:\n")
 print("\tgerman credit data:\n")
 calculate_results(test_labels_gcd_rf, y_pred_gcd_rf, delta_gcd_rf)
 print("\n\tglass:\n")
@@ -37,9 +43,15 @@ calculate_results(test_labels_glass_rf, y_pred_glass_rf, delta_glass_rf)
 # print("\n\tcovid-19:")
 # calculate_results(test_labels_covid_rf, y_pred_covid_rf, delta_covid_rf)
 
-print("Adaboost:\n")
+print("\nAdaboost:\n")
 print("\tgerman credit data:\n")
 calculate_results(test_labels_gcd_ab, y_pred_gcd_ab, delta_gcd_ab)
 print("\n\tglass:\n")
 calculate_results(test_labels_glass_ab, y_pred_glass_ab, delta_glass_ab)
+
+print("\nMultilayer perceptron:\n")
+print("\tgerman credit data:\n")
+calculate_results(test_labels_gcd_mlp, y_pred_gcd_mlp, delta_gcd_mlp)
+print("\n\tglass:\n")
+calculate_results(test_labels_glass_mlp, y_pred_glass_mlp, delta_glass_mlp)
 
--- python/models/MultiColumnLabelEncoder.py
+++ python/models/MultiColumnLabelEncoder.py
@@ -25,3 +25,32 @@ class MultiColumnLabelEncoder:
 
     def fit_transform(self,X,y=None):
         return self.fit(X,y).transform(X)
+
+class MultiColumnLabelEncoderCovidTest:
+    def __init__(self,columns = None):
+        self.columns = columns # array of column names to encode
+
+    def fit(self,X,y=None):
+        return self # not relevant here
+
+    def transform(self,X):
+        '''
+        Transforms columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transforms all
+        columns in X.
+        '''
+        output = X.copy()
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = LabelEncoder().fit_transform(output[col])
+        else:
+            for colname,col in output.iteritems():
+                col_tmp = col
+                mask = col_tmp.isnull()
+                if colname == "Province/State":
+                    print(mask)
+                output[colname] = LabelEncoder().fit_transform(col)
+        return output
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
